11bcc00fe25304cb16643311808b28bd0c8ac396
lale hüseyin <lale.huseynova.2002@gmail.com>
2022-11-13 13:38:02 +0300

    Update Student.java

--- iteration1/CSE3063F21P1_GRP16/src/Models/Student.java
+++ iteration1/CSE3063F21P1_GRP16/src/Models/Student.java
@@ -1,19 +1,27 @@
 package Models;
-
 public class Student extends Person {
-
+    
+    private StudentID studentID;
     private Advisor advisor;
-    public Student(String newName, String newLastName) {
+    //private Transcript transcript;
+    //private ArrayList<Course> selectedCourses;
+    //private Payment paymentAmount;
 
-        name = newName;
-        lastname = newLastName;
-
-
+    public StudentID getStudentID() {
+        return studentID;
     }
 
+    public void setStudentID(StudentID studentID) {
+        this.studentID = studentID;
+    }
+
+    public Advisor getAdvisor() {
+        return advisor;
+    }
 
     public void setAdvisor(Advisor advisor) {
         this.advisor = advisor;
     }
 }
+
 
