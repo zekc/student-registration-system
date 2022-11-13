1a4e32c79a43a1662da46faf4e60021604151df8
Erdem Ertürk <117317579+Erdemerturk1@users.noreply.github.com>
2022-11-13 18:09:42 +0300

    Add files via upload

---
+++ iteration1/CSE3063F21P1_GRP16/src/Models/Payment.java
@@ -0,0 +1,13 @@
+package Models;
+
+public class Payment{
+
+    private Payment paymentAmount;
+
+    public Payment getPayment(){
+        return paymentAmount;
+    }
+    public void setPayment(Payment paymentAmount){
+        this.paymentAmount= paymentAmount;
+    }
+}
