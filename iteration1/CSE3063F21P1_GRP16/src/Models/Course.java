77a952dfc0dda0b1d9f0257e7716c32c59d275be
Muhammed Said Özdemir <102900915+msoz7@users.noreply.github.com>
2022-11-15 00:34:41 +0300

    Update Course.java

--- iteration1/CSE3063F21P1_GRP16/src/Models/Course.java
+++ iteration1/CSE3063F21P1_GRP16/src/Models/Course.java
@@ -1,22 +1,80 @@
 package Models;
+import java.util.*;
 
-import Types.CourseType;
-
-import java.util.ArrayList;
 
 public class Course {
-
-
-public String coursename;
-private CourseType courseType;
-
-
-
-    public Course(String courseName, int credit, ArrayList<String> preRequisites, ArrayList<CourseSession> courseSessions, int requiredCredits, Semester courseSemester) {
-
-
-       coursename = courseName;
-
-
-    }
+	
+    private String courseName;
+    private String courseCode;
+    private int quota;
+    private String prerequisiteCourse;
+    private int credits;
+    private String courseSemesterName;
+    private String courseType ; //1..8(Semester number),FTE,NTE,UE,TE
+    private ArrayList<CourseSession> sessions= new ArrayList<CourseSession>();
+   
+    
+	public Course(String courseName, String courseCode, int quota, String prerequisiteCourse, int credits,
+			String courseSemesterName, String courseType, ArrayList<CourseSession> sessions) {
+		
+		this.courseName = courseName;
+		this.courseCode = courseCode;
+		this.quota = quota;
+		this.prerequisiteCourse = prerequisiteCourse;
+		this.credits = credits;
+		this.courseSemesterName = courseSemesterName;
+		this.courseType = courseType;
+		this.sessions = sessions;
+	}
+	public String getCourseName() {
+		return courseName;
+	}
+	public void setCourseName(String courseName) {
+		this.courseName = courseName;
+	}
+	public String getCourseCode() {
+		return courseCode;
+	}
+	public void setCourseCode(String courseCode) {
+		this.courseCode = courseCode;
+	}
+	public int getQuota() {
+		return quota;
+	}
+	public void setQuota(int quota) {
+		this.quota = quota;
+	}
+	public String getPrerequisiteCourse() {
+		return prerequisiteCourse;
+	}
+	public void setPrerequisiteCourse(String prerequisiteCourse) {
+		this.prerequisiteCourse = prerequisiteCourse;
+	}
+	public int getCredits() {
+		return credits;
+	}
+	public void setCredits(int credits) {
+		this.credits = credits;
+	}
+	public String getCourseSemesterName() {
+		return courseSemesterName;
+	}
+	public void setCourseSemesterName(String courseSemesterName) {
+		this.courseSemesterName = courseSemesterName;
+	}
+	public String getCourseType() {
+		return courseType;
+	}
+	public void setCourseType(String courseType) {
+		this.courseType = courseType;
+	}
+	public ArrayList<CourseSession> getSessions() {
+		return sessions;
+	}
+	public void setSessions(ArrayList<CourseSession> sessions) {
+		this.sessions = sessions;
+	}
+   
+    
+ 
 }
