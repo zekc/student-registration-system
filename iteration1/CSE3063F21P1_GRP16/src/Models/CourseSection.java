e2a8c7e0cbdf96df441295c029801b33a529db05
Omar Sameh Belal <belal4omar8@gmail.com>
2022-11-15 00:14:43 +0300

    Create CourseSection.java

---
+++ iteration1/CSE3063F21P1_GRP16/src/Models/CourseSection.java
@@ -0,0 +1,36 @@
+import java.util.ArrayList;
+
+public class CourseSection extends Course {
+    private ArrayList<Student> allstudent = new ArrayList<Student>();
+    private ArrayList<CoursesError> errors = new ArrayList<CoursesError>();
+
+    public ArrayList<Student> getAllstudent() {
+        return allstudent;
+    }
+
+    public void setAllstudent(ArrayList<Student> allstudent) {
+        this.allstudent = allstudent;
+    }
+
+    public ArrayList<CoursesError> getErrors() {
+        return errors;
+    }
+
+    public void setErrors(ArrayList<CoursesError> errors) {
+        this.errors = errors;
+    }
+
+    ArrayList<Course> selectedCourses = new ArrayList<Course>();
+
+    public ArrayList<Course> getSelectedCourses() {
+        return selectedCourses;
+    }
+
+    public void setSelectedCourses(ArrayList<Course> selectedCourses) {
+        this.selectedCourses = selectedCourses;
+    }
+
+    public CourseSection() {
+    }
+
+}
