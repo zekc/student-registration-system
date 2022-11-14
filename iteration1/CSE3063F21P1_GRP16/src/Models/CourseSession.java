d13099f151a57e6d5fa6bde67f56591516dcdc07
MaisSabbagh <maissabbagh20@outlook.com>
2022-11-14 21:37:26 +0300

    Update CourseSession.java

--- iteration1/CSE3063F21P1_GRP16/src/Models/CourseSession.java
+++ iteration1/CSE3063F21P1_GRP16/src/Models/CourseSession.java
@@ -1,8 +1,33 @@
 package Models;
 
+import java.sql.Time;
+
 import org.json.JSONObject;
 
 public class CourseSession {
+
+    Time LectureHour;
+    String LectureHall;
+
+    public Time getTime(){
+        return LectureHour;
+    }
+     
+    public String getLectureHall(){
+        return LectureHall;
+    }
+
+    public void setTime(Time time){
+        this.LectureHour = time;
+    }
+
+    public void setHall(String Hall){
+
+        this.LectureHall = Hall;
+
+    }
+
+
     public CourseSession(JSONObject jsonObject) {
     }
 }
