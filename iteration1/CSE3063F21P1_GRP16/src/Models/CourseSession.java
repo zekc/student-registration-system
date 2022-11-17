0d7a63a6dbdbab07ad62c40cc519c632376da58f
MaisSabbagh <102900237+MaisSabbagh@users.noreply.github.com>
2022-11-17 10:25:49 +0300

    Update CourseSession.java

--- iteration1/CSE3063F21P1_GRP16/src/Models/CourseSession.java
+++ iteration1/CSE3063F21P1_GRP16/src/Models/CourseSession.java
@@ -2,29 +2,29 @@ package Models;
 
 import org.json.JSONObject;
 
-import java.sql.Time;
+import java.sql.Time; //Java library for the Time class
 
 public class CourseSession {
 
-    Time LectureHour;
-    String LectureHall;
+    Time LectureHour;    //An attribute representing the timing of lectures
+    String LectureHall;  //An attribute representing where the lecture will be taking place 
 
     public Time getTime(){
-        return LectureHour;
+        return LectureHour;  //A get method for the time 
     }
 
 
-    public String getLectureHall(){
-        return LectureHall;
+    public String getLectureHall(){  //A get method for Lecture room
+        return LectureHall;   
     }
 
     public void setTime(Time time){
-        this.LectureHour = time;
+        this.LectureHour = time; //A set method to set/edit the timing of a lecture
     }
 
     public void setHall(String Hall){
 
-        this.LectureHall = Hall;
+        this.LectureHall = Hall;   //A set method to set/edit the room number the lecture will be taking place in
 
     }
 
